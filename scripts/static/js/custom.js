4250f6fdaa6cad88aaf486a688afc27d741b0392
rickapps <rick@rickapps.com>
2021-01-17 14:01:02 -0700

    jquery position is debugged

--- scripts/static/js/custom.js
+++ scripts/static/js/custom.js
@@ -9,22 +9,18 @@ $(function() {
       // We need the size of the selection-box so we can calculate cx,cy, and domain
       $('#xtopLeft').val(ui.position.left);
       $('#ytopLeft').val(ui.position.top);
-      $('#xcenter').val(ui.position.left + this.offsetWidth/2);
-      $('#ycenter').val(ui.position.top + this.offsetHeight/2);
+      $('#xcenter').val(ui.position.left + $(this).outerWidth()/2);
+      $('#ycenter').val(ui.position.top + $(this).outerHeight()/2);
     });
     $("#containment-wrapper").mouseover(function(){$("#selection-box h2").show()});
     $("#containment-wrapper").mouseout(function(){$("#selection-box h2").hide()});
-    $('#magnify').submit(function(){
-      // Populate our form
-      var box = $('#selection-box');
-      var pos = box.position();
-      var wid = box.width();
-      var height = box.height();
-    });
+
     var box = $('#selection-box');
-    $('#xtopLeft').val(box.position().left);
-    $('#ytopLeft').val(box.position().top);
-    $('#xcenter').val(box.position().left + box.outerWidth()/2);
-    $('#ycenter').val(box.position().top - box.outerHeight()/2);
+    var left = parseInt(box.css('left'));
+    var top = parseInt(box.css('top'));
+    $('#xtopLeft').val(left);
+    $('#ytopLeft').val(top);
+    $('#xcenter').val(left + box.outerWidth()/2);
+    $('#ycenter').val(top + box.outerHeight()/2);
   });
   
