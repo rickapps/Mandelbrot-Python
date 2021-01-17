6fd207500813dd89bdb1fe0a0d9d6bd84a13ba80
rickapps <rick@rickapps.com>
2021-01-17 14:32:17 -0700

    Revise comments

--- scripts/static/js/custom.js
+++ scripts/static/js/custom.js
@@ -4,6 +4,10 @@ $(function() {
       containment: "#containment-wrapper", scroll: false,
       stop: function( event, ui ) {}
     });
+
+    // Tried setting the fields on form submit, but position() values
+    // don't seem to be accurate. Not sure why, perhaps not recognizing
+    // parent div?.
     $("#selection-box").on( "dragstop", function( event, ui ) {
       // ui contains position and offset. Position is relative to the containment-wrapper
       // We need the size of the selection-box so we can calculate cx,cy, and domain
@@ -12,9 +16,12 @@ $(function() {
       $('#xcenter').val(ui.position.left + $(this).outerWidth()/2);
       $('#ycenter').val(ui.position.top + $(this).outerHeight()/2);
     });
+
     $("#containment-wrapper").mouseover(function(){$("#selection-box h2").show()});
     $("#containment-wrapper").mouseout(function(){$("#selection-box h2").hide()});
 
+    // To be done on document ready. Using css to get
+    // values as position method does not seem to be accurate.
     var box = $('#selection-box');
     var left = parseInt(box.css('left'));
     var top = parseInt(box.css('top'));
