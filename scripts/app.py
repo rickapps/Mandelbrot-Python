b77737720606c77abcabc47c30e9db3e864d4e73
rickapps <rick@rickapps.com>
2021-01-19 11:57:20 -0700

    Convert plot function to class

--- scripts/app.py
+++ scripts/app.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template
-from mandelbrot import drawBrot
+# Administer Mandelbrot plot form
+from flask import Flask, render_template, request
+from mandelbrot import Mandelbrot
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def plotView():
-  
-    pngImageB64String = drawBrot(-0.65, 0.0, 3.4)      
+    # A post request means we are enlarging an area of the
+    # previous plot.
+    if request.method == 'POST':
+        plot = Mandelbrot(-0.65, 0.0, 3.4)
+        pngImageB64String = plot.makeImage() 
+    else:  
+        # Display the default plot
+        plot = Mandelbrot(-0.65, 0.0, 3.4)
+        pngImageB64String = plot.makeImage() 
+
     return render_template("index.html", image=pngImageB64String)
+
+@app.route("/<int:n>", methods=["GET"])
+def plotSaved():
+    pass
 
 if __name__ == "__main__":
     app.run()
--- scripts/mandelbrot.py
+++ scripts/mandelbrot.py
@@ -5,72 +5,92 @@ import colorsys
 from io import BytesIO
 from base64 import b64encode
 
-def drawBrot(xc, yc, domain):
-    # Define some constants
-    # Number of pixels in our image.
-    # If you change the aspect ratio, change style.css 
-    imageLength = 720
-    imageHeight = 540
-    # Used to determine if series diverges
-    iterations = 100
-    testDistance = 4
+class Mandelbrot:
+    'Create an image to plot for Mandelbrot set'
+    _imageLength = 720  # Image length in pixels. Change css if you change dims
+    _imageHeight = 540  # Image height in pixels.
+    _iterations = 100   # Length of series to generate to test for convergence
+    _testDistance = 4   # Used to test for convergence
+    _saturation = 0.7   # Used for color 0 to 1. Zero full grey, One no grey
+    _brightness = 1.0   # Color brightness 0 to 1.
 
-    # Calculate the rest of our constants
-    aspectRatio = imageLength / imageHeight
-    yRange = domain / aspectRatio
-    xMin = xc - domain/2
-    yMax = yc + yRange/2 
-    xScale = domain/imageLength
-    yScale = yRange/imageHeight
+    def __init__(self, xc, yc, domain):
+        self.xc = xc
+        self.yc = yc
+        self.domain = domain
+        self.xMin = 0
+        self.yMax = 0
+        self.xScale = 0
+        self.yScale = 0
+        # Calculate xMin, yMax, xScale, yScale
+        self.setScale()
+        return
 
-    # Initialize our image to black
-    img = Image.new('RGB',(imageLength,imageHeight), color='black')
-    pixels = img.load()
+    def zoom(self, xcorner, ycorner, xcenter, ycenter):
+        'Zoom in on existing image and replot.'
+        self.xc = self.xMin + xcenter * self.xScale
+        self.yc = self.yMax - ycenter * self.yScale
+        xtemp = self.xMin + xcorner * self.xScale
+        self.domain = 2 * (xcenter - xtemp) * self.xScale
+        # Calculate new scale factors based on our new domain
+        self.setScale()
+        return
 
-    # Loop through every pixel in the image
-    for row in range(imageHeight):
-        for col in range(imageLength):
-            # Convert to (x,y) coords. (row, col) starts at top left
-            # We want min (x,y) to be at bottom left
-            xComplex = xMin + col * xScale
-            yComplex = yMax - row * yScale
-            # We are going to generate our series using this value as our constant.
-            # It is a complex number (a+bi) defined as (xComplex, yComplex)
-            # We will generate up to iterations terms of the series for
-            # each complex number. Note that (a+bi)**2 = a**2 + 2abi - b**2
-            x = xComplex # We need a starting value
-            y = yComplex # We need a starting value to generate next val in series
-            for i in range(iterations + 1):
-                a = x*x - y*y # First generate (a+bi)**2 using previous (x,y)
-                b = 2 * x * y
-                x = a + xComplex  # New series values are old value squared plus
-                y = b + yComplex  # our constant complex number.
-                # Check if series is diverging
-                if (x * x) + (y * y) > testDistance:
-                    break
 
-            # If we exited the loop early, our series diverged
-            if i < iterations:
-                # Get fraction of loop we completed. Higher values
-                # mean slower divergence
-                fraction = (i+1)/iterations
-                hue = fraction   # Between 0 and 1. Progresses Red, Yellow, Green, Cyan, Blue, Magenta
-                saturation = 0.7   # Amount of grey - zero is all grey, one is no grey
-                value = 1        # Brightness, zero is black, 1 is full brightness
-                rgb = tuple(round(i*255) for i in colorsys.hsv_to_rgb(hue,saturation,value))
-                # Set the pixel to the color we calculated
-                pixels[col,row] = rgb 
-    pngImage = BytesIO()
-    img.save(pngImage, 'PNG')
-    # Encode PNG image to base64 string
-    pngImageB64String = "data:image/png;base64,"
-    pngImageB64String += b64encode(pngImage.getvalue()).decode('utf8')
-    return pngImageB64String                     
-    # Shows image in a separate window
-    #img.show()  
-    # Shows image inline
-    #imshow(np.asarray(img))  
-    #img.save('output.png')
+    def setScale(self):
+        aspectRatio = Mandelbrot._imageLength / Mandelbrot._imageHeight
+        yRange = self.domain / aspectRatio
+        self.xMin = self.xc - self.domain/2
+        self.yMax = self.yc + yRange/2
+        self.xScale = self.domain/Mandelbrot._imageLength
+        self.yScale = yRange/Mandelbrot._imageHeight
+        return
+
+    def makeImage(self):
+        # Initialize our image to black
+        img = Image.new('RGB',(Mandelbrot._imageLength,Mandelbrot._imageHeight), color='black')
+        pixels = img.load()
+
+        # Loop through every pixel in the image
+        for row in range(Mandelbrot._imageHeight):
+            for col in range(Mandelbrot._imageLength):
+                # Convert to (x,y) coords. (row, col) starts at top left
+                # We want min (x,y) to be at bottom left
+                xComplex = self.xMin + col * self.xScale
+                yComplex = self.yMax - row * self.yScale
+                # We are going to generate our series using this value as our constant.
+                # It is a complex number (a+bi) defined as (xComplex, yComplex)
+                # We will generate up to iterations terms of the series for
+                # each complex number. Note that (a+bi)**2 = a**2 + 2abi - b**2
+                x = xComplex # We need a starting value
+                y = yComplex # We need a starting value to generate next val in series
+                for i in range(Mandelbrot._iterations + 1):
+                    a = x*x - y*y # First generate (a+bi)**2 using previous (x,y)
+                    b = 2 * x * y
+                    x = a + xComplex  # New series values are old value squared plus
+                    y = b + yComplex  # our constant complex number.
+                    # Check if series is diverging
+                    if (x * x) + (y * y) > Mandelbrot._testDistance:
+                        break
+
+                # If we exited the loop early, our series diverged
+                if i < Mandelbrot._iterations:
+                    # Get fraction of loop we completed. Higher values
+                    # mean slower divergence
+                    fraction = (i+1)/Mandelbrot._iterations
+                    # We use HSV color model here. Then we convert it to rgb.
+                    hue = fraction   # Between 0 and 1. Progresses Red, Yellow, Green, Cyan, Blue, Magenta
+                    rgb = tuple(round(i*255) for i in colorsys.hsv_to_rgb(
+                        hue, Mandelbrot._saturation, Mandelbrot._brightness))
+                    # Set the pixel to the color we calculated
+                    pixels[col,row] = rgb 
+        pngImage = BytesIO()
+        img.save(pngImage, 'PNG')
+        # Encode PNG image to base64 string
+        pngImageB64String = "data:image/png;base64,"
+        pngImageB64String += b64encode(pngImage.getvalue()).decode('utf8')
+        return pngImageB64String                     
 
 if __name__== "__main__":
-  drawBrot(-0.65, 0.0, 3.4)      
+    plot = Mandelbrot(-0.65, 0.0, 3.4)
+    plot.makeImage()
